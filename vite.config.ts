4b6eac9954bcb08d71062e92ba0deaa3fd8e6600
azertypow <azertypow@gmail.com>
2022-07-12 15:40:51 +0200

    feat: add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: mode === 'prod' ? '/les6toits.front/' : '/',
+  base: mode === 'prod' ? '/' : '/',
 })
