0ea49db0539c5099afed4d4d00fe1fd0efa028a6
azertypow <azertypow@gmail.com>
2023-02-06 10:28:12 +0100

    chore: change url

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: mode === 'prod' ? '/les6toits.front/' : '/',
+  // base: mode === 'prod' ? '/les6toits.front/' : '/',
+  base: '/',
 })
